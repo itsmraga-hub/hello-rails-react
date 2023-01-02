505c19dfcba542a30c548a9157e113240e2a9b58
Raga <ragawilliam570@gmail.com>
2023-01-02 14:24:26 +0300

    Generate static root page view

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  resources :messages
+  get 'root/index'
 
   namespace :api do
     namespace :v1 do
       resources :messages
+      get 'random_message/index'
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
