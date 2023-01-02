6c9a9d2765854190b64a7481555629b17607c1e8
Raga <ragawilliam570@gmail.com>
2023-01-02 18:33:59 +0300

    Import store and provider

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,24 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
+
+import { Provider } from "react-redux";
+import store from "./redux/configureStore";
+
+import App from "./components/App";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById('root'),
+);
+
